9fff14d2a53c16a9888910f2ec9a24b11d7d5f33
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2016-05-23 09:27:20 +0530

    file deleted for error

--- android-networking/src/main/java/com/androidnetworking/interfaces/DownloadProgressListener.java
+++
@@ -1,8 +0,0 @@
-package com.androidnetworking.interfaces;
-
-/**
- * Created by amitshekhar on 30/03/16.
- */
-public interface DownloadProgressListener {
-    void onProgress(long bytesDownloaded, long totalBytes);
-}
