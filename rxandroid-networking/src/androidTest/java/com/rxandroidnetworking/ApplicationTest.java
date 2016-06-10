99ef5e9bc552bed0ffba26e3f99ce6d3fe32b836
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2016-06-10 15:22:41 +0530

    start for rx support

--- rxandroid-networking/src/androidTest/java/com/rxandroidnetworking/ApplicationTest.java
+++ rxandroid-networking/src/androidTest/java/com/rxandroidnetworking/ApplicationTest.java
@@ -15,4 +15,16 @@
  *    limitations under the License.
  */
 
-include ':app', ':android-networking', ':mylibrary', ':rxandroid-networking'
+package com.rxandroidnetworking;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
--- rxandroid-networking/src/test/java/com/rxandroidnetworking/ExampleUnitTest.java
+++ rxandroid-networking/src/test/java/com/rxandroidnetworking/ExampleUnitTest.java
@@ -15,4 +15,18 @@
  *    limitations under the License.
  */
 
-include ':app', ':android-networking', ':mylibrary', ':rxandroid-networking'
+package com.rxandroidnetworking;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
