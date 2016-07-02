a68fab630270523ede22b0dbfbfb06c3c6099587
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2016-07-02 22:22:38 +0530

    android networking gson started for parsing inbuilt - optional

--- android-networking-gson/src/androidTest/java/com/androidnetworkinggson/ApplicationTest.java
+++ android-networking-gson/src/androidTest/java/com/androidnetworkinggson/ApplicationTest.java
@@ -15,4 +15,16 @@
  *    limitations under the License.
  */
 
-include ':app', ':android-networking', ':rxandroid-networking', ':android-networking-gson'
+package com.androidnetworkinggson;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
--- android-networking-gson/src/test/java/com/androidnetworkinggson/ExampleUnitTest.java
+++ android-networking-gson/src/test/java/com/androidnetworkinggson/ExampleUnitTest.java
@@ -15,4 +15,18 @@
  *    limitations under the License.
  */
 
-include ':app', ':android-networking', ':rxandroid-networking', ':android-networking-gson'
+package com.androidnetworkinggson;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
