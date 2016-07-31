724d3182d2e770091af00adca95794bd2892fe5d
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2016-07-31 13:10:17 +0530

    parserAdded

---
+++ android-networking/src/main/java/com/androidnetworking/interfaces/Parser.java
@@ -0,0 +1,47 @@
+/*
+ *
+ *  *    Copyright (C) 2016 Amit Shekhar
+ *  *    Copyright (C) 2011 Android Open Source Project
+ *  *
+ *  *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  *    you may not use this file except in compliance with the License.
+ *  *    You may obtain a copy of the License at
+ *  *
+ *  *        http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *    Unless required by applicable law or agreed to in writing, software
+ *  *    distributed under the License is distributed on an "AS IS" BASIS,
+ *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *    See the License for the specific language governing permissions and
+ *  *    limitations under the License.
+ *
+ */
+
+package com.androidnetworking.interfaces;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+
+/**
+ * Created by amitshekhar on 31/07/16.
+ */
+public interface Parser<F, T> {
+
+    T convert(F value) throws IOException;
+
+    abstract class Factory {
+
+        public Parser<ResponseBody, ?> responseBodyParser(Type type) {
+            return null;
+        }
+
+        public Parser<?, RequestBody> requestBodyParser(Type type) {
+            return null;
+        }
+
+    }
+
+}
