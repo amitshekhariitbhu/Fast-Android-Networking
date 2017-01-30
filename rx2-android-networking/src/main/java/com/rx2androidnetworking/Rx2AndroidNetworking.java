3a3390b8d2f99fb71998fda063192b3c4c0e8479
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2017-01-30 13:55:58 +0530

    Module created for Rx2AndroidNetworking

---
+++ rx2-android-networking/src/main/java/com/rx2androidnetworking/Rx2AndroidNetworking.java
@@ -0,0 +1,38 @@
+/*
+ *
+ *  *    Copyright (C) 2016 Amit Shekhar
+ *  *    Copyright (C) 2011 Android Open Source Project
+ *  *
+ *  *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  *    you may not use this file except in compliance with the License.
+ *  *    You may obtain a copy of the License at
+ *  *
+ *  *        http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *    Unless required by applicable law or agreed to in writing, software
+ *  *    distributed under the License is distributed on an "AS IS" BASIS,
+ *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *    See the License for the specific language governing permissions and
+ *  *    limitations under the License.
+ *
+ */
+
+package com.rx2androidnetworking;
+
+/**
+ * Created by amitshekhar on 30/01/17.
+ */
+
+/**
+ * Rx2AndroidNetworking entry point.
+ * You must initialize this class before use. The simplest way is to just do
+ * {#code AndroidNetworking.initialize(context)}.
+ */
+public class Rx2AndroidNetworking {
+
+    /**
+     * private constructor to prevent instantiation of this class
+     */
+    private Rx2AndroidNetworking() {
+    }
+}
