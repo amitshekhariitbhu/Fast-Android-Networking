ef0d78956459687ce8fc0a439cadba9cfcc6efea
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2017-01-31 23:00:55 +0530

    Remove unused testing dependencies

--- rxsampleapp/src/main/java/com/rxsampleapp/RxMyApplication.java
+++ rxsampleapp/src/main/java/com/rxsampleapp/RxMyApplication.java
@@ -36,13 +36,6 @@ public class RxMyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         appInstance = this;
-        //For testing purpose only: network interceptor : enable it only for non-images request checking
-//        Stetho.initializeWithDefaults(getApplicationContext());
-//        OkHttpClient okHttpClient = new OkHttpClient().newBuilder().addNetworkInterceptor(new StethoInterceptor()).addInterceptor(new GzipRequestInterceptor()).build();
-//        OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
-//                .addInterceptor(new GzipRequestInterceptor())
-//                .build();
-//        AndroidNetworking.initialize(getApplicationContext(), okHttpClient);
         AndroidNetworking.initialize(getApplicationContext());
         AndroidNetworking.enableLogging();
         AndroidNetworking.setConnectionQualityChangeListener(new ConnectionQualityChangeListener() {
