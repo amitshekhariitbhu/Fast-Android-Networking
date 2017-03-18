c026dcdd6e3329033912d2ca1b6908df436f6c38
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2017-03-18 12:37:54 +0530

    Fix progress bug, week ref gets gc

--- android-networking/src/main/java/com/androidnetworking/internal/DownloadProgressHandler.java
+++ android-networking/src/main/java/com/androidnetworking/internal/DownloadProgressHandler.java
@@ -25,28 +25,25 @@ import com.androidnetworking.common.ANConstants;
 import com.androidnetworking.interfaces.DownloadProgressListener;
 import com.androidnetworking.model.Progress;
 
-import java.lang.ref.WeakReference;
-
 /**
  * Created by amitshekhar on 24/05/16.
  */
 public class DownloadProgressHandler extends Handler {
 
-    private final WeakReference<DownloadProgressListener> mDownloadProgressListenerWeakRef;
+    private final DownloadProgressListener mDownloadProgressListenerWeakRef;
 
     public DownloadProgressHandler(DownloadProgressListener downloadProgressListener) {
         super(Looper.getMainLooper());
-        mDownloadProgressListenerWeakRef = new WeakReference<>(downloadProgressListener);
+        mDownloadProgressListenerWeakRef = downloadProgressListener;
     }
 
     @Override
     public void handleMessage(Message msg) {
-        final DownloadProgressListener downloadProgressListener = mDownloadProgressListenerWeakRef.get();
         switch (msg.what) {
             case ANConstants.UPDATE:
-                if (downloadProgressListener != null) {
+                if (mDownloadProgressListenerWeakRef != null) {
                     final Progress progress = (Progress) msg.obj;
-                    downloadProgressListener.onProgress(progress.currentBytes, progress.totalBytes);
+                    mDownloadProgressListenerWeakRef.onProgress(progress.currentBytes, progress.totalBytes);
                 }
                 break;
             default:
